b630b48800c194c6f95b0059107b9864ba2e8c7c
qu-lan <109791086+qu-lan@users.noreply.github.com>
2022-07-27 10:37:59 +0800

    uadata
    updata

--- first.py
+++ first.py
@@ -1,3 +1,14 @@
 def 1st():
     print('hello world !')
     print('this is a test_py')
+
+print('steps:')
+'''
+if anything changed in 工作区 , I should use git add to add news into  暂存区
+and then if there is no mistakes, I can use git commit -am info to 本地仓库
+just use push it to remote 仓库
+
+and use git fetch to fetch remote 仓库 to 本地仓库
+use git diff to find the different between 工作区 and 本地仓库 
+if all is ok, 合并them
+'''
